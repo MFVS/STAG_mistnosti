9a8580a57c06162a6e1f2e184de032c79919a44e
yoptohlejepeta <peta.kotlan@seznam.cz>
2023-11-29 23:34:21 +0100

    kostlivec

--- app/main.py
+++ app/main.py
@@ -1,66 +1,65 @@
-import streamlit as st
-import httpx
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 from io import StringIO
 import pandas as pd
+import requests
 
 
-st.set_page_config(layout="wide", page_title="STAG Místnosti", page_icon="🏫")
-st.title("STAG Místnosti")
-
-st.header("Seznam místností")
-
-url_mistnosti_info = "https://ws.ujep.cz/ws/services/rest2/mistnost/getMistnostiInfo"
-vars_mistnosti = {
-    "zkrBudovy": "CP",
-    "pracoviste": "%",
-    "typ": "%",
-    "cisloMistnosti": "%",
-    "outputFormatEncoding": "utf-8",
-    "outputFormat": "CSV",
-}
+app = FastAPI()
+templates = Jinja2Templates(directory="app/templates")
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
 
 
-response = httpx.get(url_mistnosti_info, params=vars_mistnosti)
-
-df_mistnosti = pd.read_csv(StringIO(response.text), sep=";")
-# cisloMistnosti starts with 6. or 7.
-
-df_mistnosti = df_mistnosti[df_mistnosti["cisloMistnosti"].str.startswith(("6.", "7."))]
-# order by cisloMistnosti
-df_mistnosti.sort_values(by=["cisloMistnosti"], inplace=True)
-
-st.dataframe(df_mistnosti)
-
-st.header("Rozvrhy místností")
-
-# df_mistnosti["typ"] is Učebna or Laboratoř
-df_mistnosti = df_mistnosti[df_mistnosti["typ"].isin(["Učebna", "Laboratoř"])]
-
-url_rozvrh = "https://ws.ujep.cz/ws/services/rest2/rozvrhy/getRozvrhByMistnost"
-
-for mistnost in df_mistnosti["cisloMistnosti"]:
-    st.subheader(mistnost, anchor=mistnost)
-    with st.sidebar:
-        st.markdown(
-            f"<a href = #{mistnost} style='text-decoration: none; font-size: 1.5em;'><span style='transition: color 0.3s;'>{mistnost}</span></a>",
-            unsafe_allow_html=True,
-        )
-
-    vars_rozvrh = {
-        "semestr": "%",
-        "mistnost": mistnost,
-        "vsechnyCasyKonani": "true",
-        "budova": "CP",
-        "jenRozvrhoveAkce": "false",
-        "vsechnyAkce": "true",
-        "jenBudouciAkce": "false",
-        "rok": "2023",
-        "lang": "cs",
+@app.get("/")
+def read_root(request: Request):
+    url_mistnosti_info = "https://ws.ujep.cz/ws/services/rest2/mistnost/getMistnostiInfo"
+    vars_mistnosti = {
+        "zkrBudovy": "CP",
+        "pracoviste": "%",
+        "typ": "%",
+        "cisloMistnosti": "%",
         "outputFormatEncoding": "utf-8",
         "outputFormat": "CSV",
     }
+
+    response = requests.get(url_mistnosti_info, params=vars_mistnosti)
+
+    df_mistnosti = pd.read_csv(StringIO(response.text), sep=";")
+    # cisloMistnosti starts with 6. or 7.
+
+    df_mistnosti = df_mistnosti[df_mistnosti["cisloMistnosti"].str.startswith(("6.", "7."))]
+    # order by cisloMistnosti
+    df_mistnosti.sort_values(by=["cisloMistnosti"], inplace=True)
+
+    # df_mistnosti["typ"] is Učebna or Laboratoř
+    df_mistnosti = df_mistnosti[df_mistnosti["typ"].isin(["Učebna", "Laboratoř"])]
+
+    print(df_mistnosti.columns)
+    # url_rozvrh = "https://ws.ujep.cz/ws/services/rest2/rozvrhy/getRozvrhByMistnost"
+
+    # for mistnost in df_mistnosti["cisloMistnosti"]:
+    #     with st.sidebar:
+    #         st.markdown(
+    #             f"<a href = #{mistnost} style='text-decoration: none; font-size: 1.5em;'><span style='transition: color 0.3s;'>{mistnost}</span></a>",
+    #             unsafe_allow_html=True,
+    #         )
+
+    #     vars_rozvrh = {
+    #         "semestr": "%",
+    #         "mistnost": mistnost,
+    #         "vsechnyCasyKonani": "true",
+    #         "budova": "CP",
+    #         "jenRozvrhoveAkce": "false",
+    #         "vsechnyAkce": "true",
+    #         "jenBudouciAkce": "false",
+    #         "rok": "2023",
+    #         "lang": "cs",
+    #         "outputFormatEncoding": "utf-8",
+    #         "outputFormat": "CSV",
+    #     }
+        
+    #     response = httpx.get(url_rozvrh, params=vars_rozvrh)
+    #     df_rozvrh = pd.read_csv(StringIO(response.text), sep=";")
     
-    response = httpx.get(url_rozvrh, params=vars_rozvrh)
-    df_rozvrh = pd.read_csv(StringIO(response.text), sep=";")
-    
-    st.dataframe(df_rozvrh)
+    return templates.TemplateResponse("index.html", {"request": request, "df_mistnosti": df_mistnosti})
